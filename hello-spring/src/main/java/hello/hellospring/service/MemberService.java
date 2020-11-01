7aa13b39ea715177f65804c2fc286f3fc8ce19c9
LemonYellowParrot <ljy012900@gmail.com>
2020-11-01 20:39:50 +0900

    Week3&Week4

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,17 @@ package hello.hellospring.service;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+//@Service
 public class MemberService {
     private final MemberRepository memberRepository;
     //command shift t시 바로 테스트 생성
-
+    //@Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,37 +1,26 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class MemberServiceTest {
+class MemberServiceIntegrationTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
-
-    @BeforeEach
-    public void beforeEach(){
-        //여기 memberRepository 한번에 하는 법 오류났음.
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    @AfterEach //테스트 1개 끝날 때마다 리셋
-    public void afterEach(){
-        memberRepository.clearStore();
-    }
-    //memberrepository와 같은 것, 다른 인스턴스여서 내용 달라지고 그럴 수 있음. 다른 레포로 테스트해.
-    //memberservice에서 repo를 외부에서 넣어주도록 바꿈.
 
     @Test
-    public void 회원가입() { //테스트는 한글로적어도 상관 x
+    public void 회원가입() { //한글도ok
         //given
         Member member = new Member();
         member.setName("hello");
@@ -76,4 +65,5 @@ class MemberServiceTest {
     @Test
     public void findOne() {
     }
+
 }
