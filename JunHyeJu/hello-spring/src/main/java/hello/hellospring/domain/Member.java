55c2e5f85f75cc35b9378ab734f2c3d728d34005
hyeju1123 <hjjun1123@gmail.com>
2020-11-02 06:53:54 +0900

    add week4 assignment

--- JunHyeJu/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ JunHyeJu/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
 
     public long getId() {
